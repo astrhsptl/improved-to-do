3fcc22313b85439dfb6f4b70b37c16231bc10aae
Alexander Kizimenko <dctrback@bk.ru>
2024-03-27 08:23:44 +0300

    asdf

---
+++ src/frontend/src/features/format-swiper/index.tsx
@@ -0,0 +1,10 @@
+export const FormatSwiper = () => {
+  return (
+    <div>
+      <span>Day</span>
+      <span>Week</span>
+      <span>Month</span>
+      <span>Year</span>
+    </div>
+  );
+};
--- src/frontend/src/widgets/calendar/head.tsx
+++ src/frontend/src/widgets/calendar/head.tsx
@@ -1,5 +1,27 @@
 import { CalendarHeadStyles } from '@/shared';
+import { clsx } from 'clsx';
+import { useState } from 'react';
 
 export const CalendarHead = () => {
-  return <section className={CalendarHeadStyles.headLayout}></section>;
+  const [active, setActive] = useState(0);
+
+  return (
+    <section className={CalendarHeadStyles.headLayout}>
+      <div className={CalendarHeadStyles.swiper}>
+        <span
+          className={clsx(
+            CalendarHeadStyles.swiperItem,
+            CalendarHeadStyles.active,
+          )}
+        >
+          Day
+        </span>
+        <span>Week</span>
+        <span>Month</span>
+        <span>Year</span>
+      </div>
+
+      <div>U</div>
+    </section>
+  );
 };
